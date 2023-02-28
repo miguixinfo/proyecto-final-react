3369eb0bc4b2feff2f5e120fee45497f1f23263a
miguixinfo <miguixinfo@gmail.com>
2023-02-28 10:47:39 +0100

    Añadimos personajes.

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-function Peliculas() {
-  const seriesUrl = 'http://gateway.marvel.com/v1/public/series?&ts=1&apikey=ad6ea905acb56b4f31146d812a2568a1&hash=e666c45f929cb194ce2111c743dc3ff9';
+function Characters() {
+  const charactersUrl = 'http://gateway.marvel.com/v1/public/characters?&ts=1&apikey=ad6ea905acb56b4f31146d812a2568a1&hash=e666c45f929cb194ce2111c743dc3ff9';
 
-  const [series, setSeries] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-  const fetchSeries = (seriesUrl) => {
-    fetch(seriesUrl)
+  const fetchCharacters = (charactersUrl) => {
+    fetch(charactersUrl)
       .then((response) => response.json())
       .then((data) => {
-        setSeries(data.data.results);
+        setCharacters(data.data.results);
       })
       .catch((error) => console.log(error));
   };
 
   useEffect(() => {
-    fetchSeries(seriesUrl);
+    fetchCharacters(charactersUrl);
   }, []);
   return (
     <div className="container">
       <div className="row">
-        {series.map((item) => (
+        {characters.map((item) => (
           <div className="col">
             <div className="card mt-4 text-center">
               <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} width="280px" height="300px" alt="#" />
-              <h4 className="card-title">{item.title}</h4>
+              <h4 className="card-title">{item.name}</h4>
             </div>
           </div>
         ))}
@@ -34,4 +34,4 @@ function Peliculas() {
   );
 }
 
-export default Peliculas;
+export default Characters;
