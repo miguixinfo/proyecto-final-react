d9df85b3f695ac95e790a7ca996e7847e329af2f
Matias <alumno.532989@ies-azarquiel.es>
2023-02-28 10:49:52 +0100

    añado footer casi terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,105 @@
 import React from 'react';
+import imgLogo from '../assets/marvel-logo-minimalista.png';
 
 function Footer() {
   return (
 
-    <h1>Footer</h1>
+    <div className="text-center text-lg-start bg-negro border-foot text-light">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="container text-center ">
+          <a className="btn text-white btn-floating btn-circle btn-foot mx-3 my-" href="#!">
+            <i className="fab fa-facebook" />
+          </a>
+          <a className="btn text-white btn-floating btn-circle btn-foot mx-3 my-" href="#!">
+            <i className="fab fa-twitter" />
+          </a>
+          <a className="btn text-white btn-floating btn-circle btn-foot mx-3 my-" href="#!">
+            <i className="fab fa-google" />
+          </a>
+          <a className="btn text-white btn-floating btn-circle btn-foot mx-3 my-" href="#!">
+            <i className="fab fa-instagram" />
+          </a>
+          <a className="btn text-white btn-floating btn-circle btn-foot mx-3 my-" href="#!">
+            <i className="fab fa-linkedin-in" />
+          </a>
+          <a className="btn text-white btn-floating btn-circle btn-foot mx-3 my-" href="#!">
+            <i className="fab fa-github" />
+          </a>
+        </div>
+      </section>
+
+      <section className="">
+        <div className="container text-center text-md-start mt-5">
+          <div className="row mt-3">
+            <div className="col-md-3 col-lg-4 col-xl-3 ml-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                <img src={imgLogo} alt="" width="150" height="70" />
+              </h6>
+
+            </div>
+            <div className="col-md-3 col-lg-2 col-xl-2 mr-5 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                Paginas legales
+              </h6>
+              <p>
+                <a href="paginaslegales.html" className="text-reset text-decoration-none">Aviso legal</a>
+              </p>
+              <p>
+                <a href="paginaslegales.html" className="text-reset text-decoration-none">Política de privacidad</a>
+              </p>
+              <p>
+                <a href="paginaslegales.html" className="text-reset text-decoration-none">Política de cookies</a>
+              </p>
+            </div>
+            <div className="col-md-2 col-lg-2 col-xl-2 mr-5 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                Productos
+              </h6>
+              <p>
+                <a href="https://angular.io/" className="text-reset text-decoration-none">Series</a>
+              </p>
+              <p>
+                <a href="https://es.reactjs.org/" className="text-reset text-decoration-none">Juguetes</a>
+              </p>
+              <p>
+                <a href="https://vuejs.org/" className="text-reset text-decoration-none">Videojuegos</a>
+              </p>
+              <p>
+                <a href="https://laravel.com/" className="text-reset text-decoration-none">Películas</a>
+              </p>
+            </div>
+            <div className="col-md-4 col-lg-3 col-xl-3 mb-md-0 mb-4 mr-5">
+              <h6 className="text-uppercase fw-bold mb-4">Contacto</h6>
+              <p>
+                <i className="fas fa-home me-3 text-secondary" />
+                {' '}
+                toeldo
+              </p>
+              <p>
+                <i className="fas fa-envelope me-3 text-secondary" />
+                info@example.com
+              </p>
+              <p>
+                <i className="fas fa-phone me-3 text-secondary" />
+                {' '}
+                +34 925 234 837
+              </p>
+              <p>
+                <i className="fas fa-print me-3 text-secondary" />
+                {' '}
+                +34 925 234 837
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="text-center p-4">
+        <p>
+          © 2022 Copyright:
+          <a className="text-reset fw-bold text-decoration-none" href="https://www.marvel.com/"> marvel.com</a>
+        </p>
+      </div>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ImgHeader from '../assets/descarga.png';
+import '../index.css';
 
 function Navbar() {
   return (
-    <nav className="navbar bg-danger">
+    <nav className="navbar bg-rojomarvel">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 text-white ">MARVEL</span>
-        <ul className="ul-nav">
-          <li className="li-nav"><NavLink className="li-nav-navlink px-2" to="/">Home</NavLink></li>
-          <li className="li-nav"><NavLink className="li-nav-navlink px-2" to="/comics">Comics</NavLink></li>
-          <li className="li-nav"><NavLink className="li-nav-navlink px-2" to="/peliculas">Películas</NavLink></li>
+        <img src={ImgHeader} alt="" width="150" height="70" />
+        <ul className="ul-nav ">
+          <li className="li-nav"><NavLink className="li-nav-navlink btn border third mx-2" to="/">Home</NavLink></li>
+          <li className="li-nav"><NavLink className="li-nav-navlink btn border third mx-2" to="/comics">Comics</NavLink></li>
+          <li className="li-nav"><NavLink className="li-nav-navlink btn border third mx-2" to="/peliculas">Películas</NavLink></li>
         </ul>
       </div>
     </nav>
