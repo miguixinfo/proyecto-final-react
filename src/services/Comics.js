4d4103ee8408f961b456ddde35e2d3367abba7ec
Danimople <alumno.312059@ies-azarquiel.es>
2023-03-01 18:21:10 +0100

    cambios del js

--- src/services/Comics.js
+++ src/services/Comics.js
@@ -1,18 +1,13 @@
-import { useState, useEffect } from 'react';
+const URL = 'http://gateway.marvel.com/v1/public/comics?&ts=1&apikey=ad6ea905acb56b4f31146d812a2568a1&hash=e666c45f929cb194ce2111c743dc3ff9';
 
-const comicsUrl = 'http://gateway.marvel.com/v1/public/comics?&ts=1&apikey=ad6ea905acb56b4f31146d812a2568a1&hash=e666c45f929cb194ce2111c743dc3ff9';
+export async function getComics() {
+  const respuesta = await fetch(`${URL}`);
+  const personas = await respuesta.json();
+  return personas;
+}
 
-const [comics, setComics] = useState([]);
-
-const fetchComics = (comicsUrl) => {
-  fetch(comicsUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      setComics(data.data.results);
-    })
-    .catch((error) => console.log(error));
-};
-
-useEffect(() => {
-  fetchComics(comicsUrl);
-}, []);
+export async function getComic(id) {
+  const respuesta = await fetch(`${URL}/${id}`);
+  const person = await respuesta.json();
+  return person;
+}
