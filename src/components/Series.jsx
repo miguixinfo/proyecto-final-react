159b461cba1365d7d6425284e49a32009d47821a
miguixinfo <miguixinfo@gmail.com>
2023-03-02 12:45:31 +0100

    Añadimos funcionalidad en los servicios y nos ahorramos líneas de código.

--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { getSeries } from '../services/Series';
 
 function Peliculas() {
   const seriesUrl = 'http://gateway.marvel.com/v1/public/series?&ts=1&apikey=ad6ea905acb56b4f31146d812a2568a1&hash=e666c45f929cb194ce2111c743dc3ff9';
@@ -6,14 +7,9 @@ function Peliculas() {
   const [series, setSeries] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
-  const fetchSeries = (seriesUrl) => {
-    fetch(seriesUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        setSeries(data.data.results);
-      })
-      .catch((error) => console.log(error));
-  };
+  useEffect(() => {
+    getSeries().then((data) => setSeries(data.data.results));
+  }, []);
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -23,9 +19,6 @@ function Peliculas() {
     ? series
     : series.filter((item) => item.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
-  useEffect(() => {
-    fetchSeries(seriesUrl);
-  }, []);
   return (
     <div className="container mb-4">
       <div className="row">
