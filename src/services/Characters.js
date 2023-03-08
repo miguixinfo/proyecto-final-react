5778d964962591cb5c405609800aace15f1a6c72
miguixinfo <miguixinfo@gmail.com>
2023-03-08 10:05:39 +0100

    Añadimos enlace desde personaje a comic con un split de la url del comic.

--- src/services/Characters.js
+++ src/services/Characters.js
@@ -2,6 +2,7 @@ const URL = 'http://gateway.marvel.com/v1/public/characters?&ts=1&apikey=ad6ea90
 
 const urlCharacters = 'http://gateway.marvel.com/v1/public/characters';
 const urlKey = '?&ts=1&apikey=ad6ea905acb56b4f31146d812a2568a1&hash=e666c45f929cb194ce2111c743dc3ff9';
+const urlComics = 'http://gateway.marvel.com/v1/public/comics';
 
 export async function getCharacters() {
   const respuesta = await fetch(`${URL}`);
