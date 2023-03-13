fec79547249edab6edc347e2764e99a5826f7c16
Matias <alumno.532989@ies-azarquiel.es>
2023-03-13 10:41:41 +0100

    Error Page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import ImgDeadpool from '../assets/pngdeadpool.png';
+import '../index.css';
 
 function Error() {
   return (
-
-    <h1>Error 404: Página no encontrada</h1>
+    <div className="div">
+      <div className="row mt-5">
+        <div className="col-5">
+          <img src={ImgDeadpool} alt="" />
+        </div>
+        <div className="col-7  ">
+          <h1 className="font--404 ">404 </h1>
+          <h1 className="font--error "> Página no encontrada</h1>
+          <p>La página a la que está intentando acceder no existe.</p>
+          <p>
+            Para volver a inicio pulse
+            {' '}
+            <NavLink className="link-cards" to="/">Aquí</NavLink>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
